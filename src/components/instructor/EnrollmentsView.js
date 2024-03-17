5b34912d453ad57a440a9393a0390b6b1cba5d2e
Anthony Broussard <anbroussard@csumb.edu>
2024-03-16 18:40:14 -0700

    Did some work to the EnrollmentsView, but it's not currently working. I'm speculating that maybe the enrollment backend isn't working. Using postman, I'm still unable to generate a working enrollment query.

--- src/components/instructor/EnrollmentsView.js
+++ src/components/instructor/EnrollmentsView.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {useLocation} from 'react-router-dom';
+import { SERVER_URL } from '../../Constants';
 
 // instructor view list of students enrolled in a section 
 // use location to get section no passed from InstructorSectionsView
@@ -10,14 +11,60 @@ import {useLocation} from 'react-router-dom';
 //  hint:  <input type="text" name="grade" value={e.grade} onChange={onGradeChange} />
 
 const EnrollmentsView = (props) => {
+    const headers = ['EnrollmentId', 'StudentId', 'Name', 'Email', 'Grade']
+
+    const [enrollments, setEnrollments] = useState([]);
+    const [message, setMessage] = useState('');
+
+    const onGradeChange = (event) => {
+        setEnrollments({...enrollments, [event.target.name]:event.target.value});
+    }
 
     const location = useLocation();
     const {secNo, courseId, secId} = location.state;
 
+    const fetchEnrollments = async () => {
+        try {
+            const response = await fetch(`${SERVER_URL}/sections/` + secNo + `/enrollments`);
+            if (response.ok) {
+                const enrollments = await response.json();
+                setEnrollments(enrollments);
+            } else {
+                const json = await response.json();
+                setMessage("response error: " + json.message + location.secNo);
+            }
+        } catch (err) {
+            setMessage("network error: " + err);
+        }
+    }
+
+    useEffect( () => {
+        fetchEnrollments();
+    }, []);
+
     return(
-        <> 
-            <h3>Not implemented</h3>   
-        </>
+        <div> 
+            <h3>Enrollments</h3>   
+            <h4>{message}</h4>
+            <table className="Center">
+                <thead>
+                    <tr>
+                        {headers.map((e, idx) => (<th key={idx}>{e}</th>))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {enrollments.map((e) => (
+                        <tr key = {e.enrollmentId}>
+                            <td>{e.enrollmentId}</td>
+                            <td>{e.studentId}</td>
+                            <td>{e.name}</td>
+                            <td><input type="text" name="grade" value={e.grade} onChange={onGradeChange} /></td>
+                            <td></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
 
