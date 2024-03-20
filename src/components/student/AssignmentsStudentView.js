73a8147a1741b98e47f7dc3aa24de838281c8c1d
Anthony Broussard <anbroussard@csumb.edu>
2024-03-19 19:27:57 -0700

    Updated functionality for AssignmentsStudentView

--- src/components/student/AssignmentsStudentView.js
+++ src/components/student/AssignmentsStudentView.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import { SERVER_URL } from '../../Constants';
 
 // student views a list of assignments and assignment grades 
 // use the URL  /assignments?studentId= &year= &semester=
@@ -8,12 +9,53 @@ import React, {useState} from 'react';
 // display a table with columns  Course Id, Assignment Title, Assignment DueDate, Score
 
 const AssignmentsStudentView = (props) => {
+    const headers = ['CourseId', 'Assignment Title', 'Assignment DueDate', 'Score'];
+
+    const [assignments, setAssignments] = useState([]);
+    const [message, setMessage] = useState('');
     
-     
+    const fetchAssignments = async () => {
+        try {
+            const response = await fetch(`${SERVER_URL}/assignments?studentId=3&year=2024&semester=Spring`);
+            if (response.ok) {
+                const assignments = await response.json();
+                setAssignments(assignments);     
+                
+            } else {
+                const json = await response.json();
+                setMessage("reponse error: " + json.message);
+            }
+        } catch (err) {
+            setMessage("network error: " + err);
+        }        
+    }
+    
+    useEffect( () => {
+        fetchAssignments();
+    }, []);
+
     return(
-        <> 
-            <h3>Not implemented</h3>   
-        </>
+        <div> 
+           <h3>Assignments</h3>
+           <h4>{message}</h4>
+           <table className="Center"> 
+            <thead>
+                <tr>
+                    {headers.map((a, idx) => (<th key={idx}>{a}</th>))}
+                </tr>
+            </thead>
+            <tbody>
+                {assignments.map((a) => (
+                    <tr key = {a.courseId}>
+                        <td>{a.courseId}</td>
+                        <td>{a.title}</td>
+                        <td>{a.dueDate}</td>
+                        <td>{a.score}</td>
+                    </tr>
+                ))}
+            </tbody>
+           </table>
+        </div>
     );
 }
 
